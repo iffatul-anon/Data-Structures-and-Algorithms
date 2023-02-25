0d06edd1fb76441b9302e982927b3138d342b4ea
Md. Iffatul Islam Anon <105257268+iffatul-anon@users.noreply.github.com>
2023-02-26 04:47:57 +0600

    Algorithm for Merge Sort

---
+++ Merge_Sort.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void merge(int a[], int low, int mid, int high)
+{
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+    int b[high + 1];
+    while (i <= mid && j <= high)
+    {
+        if (a[i] <= a[j])
+        {
+            b[k] = a[i];
+            i++;
+        }
+        else
+        {
+            b[k] = a[j];
+            j++;
+        }
+        k++;
+    }
+    while (i <= mid)
+    {
+        b[k] = a[i];
+        i++;
+        k++;
+    }
+    while (j <= high)
+    {
+        b[k] = a[j];
+        j++;
+        k++;
+    }
+    for (k = low; k <= high; k++)
+    {
+        a[k] = b[k];
+    }
+}
+
+void Msort(int a[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = low + (high - low) / 2;
+        Msort(a, low, mid);
+        Msort(a, mid + 1, high);
+        merge(a, low, mid, high);
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    Msort(a, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
