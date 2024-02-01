38e90905fb6d5cfb6a4910d4ba1f21745f791196
Md. Iffatul Islam Anon <105257268+iffatul-anon@users.noreply.github.com>
2024-02-02 00:12:33 +0600

    Add files via upload

---
+++ 14.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<math.h>
+using namespace std;
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        
+    }
+}
